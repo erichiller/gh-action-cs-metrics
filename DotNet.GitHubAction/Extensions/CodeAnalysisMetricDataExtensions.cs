f5acbb979774ec449bf27313d27f109dd6a40f21
Eric D. Hiller <eric@hiller.pro>
2023-10-22 06:45:36 -0500

    Fix method signature when class and/or return type are generic with multiple type parameters

--- DotNet.GitHubAction/Extensions/CodeAnalysisMetricDataExtensions.cs
+++ DotNet.GitHubAction/Extensions/CodeAnalysisMetricDataExtensions.cs
@@ -3,16 +3,14 @@ using System.Text.RegularExpressions;
 
 namespace DotNet.GitHubAction.Extensions;
 
-static class CodeAnalysisMetricDataExtensions
-{
+static class CodeAnalysisMetricDataExtensions {
     internal static string ToCyclomaticComplexityEmoji(this CodeAnalysisMetricData metric) =>
-        metric.CyclomaticComplexity switch
-        {
-            >= 0 and <= 7 => "✅",                 // ✅ (was ✔️)
-            8 or 9 => "⚠️",                        // ⚠️
-            10 or 11 => "☢️",                      // ☢️
-            >= 12 and <= 14 => "❌",               // ❌
-            _ => "🤯"                              // 🤯
+        metric.CyclomaticComplexity switch {
+            >= 0 and <= 7   => "✅",  // ✅ (was ✔️)
+            8 or 9          => "⚠️", // ⚠️
+            10 or 11        => "☢️", // ☢️
+            >= 12 and <= 14 => "❌",  // ❌
+            _               => "🤯"  // 🤯
         };
 
     internal static int CountNamespaces(this CodeAnalysisMetricData metric) =>
@@ -23,97 +21,73 @@ static class CodeAnalysisMetricDataExtensions
 
     static int CountKind(this CodeAnalysisMetricData metric, SymbolKind kind) =>
         metric.Children
-            .Flatten(child => child.Children)
-            .Count(child => child.Symbol.Kind == kind);
+              .Flatten(child => child.Children)
+              .Count(child => child.Symbol.Kind == kind);
 
-    internal static (int Complexity, string Emoji) FindHighestCyclomaticComplexity(
-        this CodeAnalysisMetricData metric) =>
+    internal static (int Complexity, string Emoji) FindHighestCyclomaticComplexity(this CodeAnalysisMetricData metric) =>
         metric.Children
-            .Flatten(child => child.Children)
-            .Where(child =>
-                child.Symbol.Kind is not SymbolKind.Assembly
-                and not SymbolKind.Namespace
-                and not SymbolKind.NamedType)
-            .Select(m => (Metric: m, m.CyclomaticComplexity))
-            .OrderByDescending(_ => _.CyclomaticComplexity)
-            .Select(_ => (_.CyclomaticComplexity, _.Metric.ToCyclomaticComplexityEmoji()))
-            .FirstOrDefault();
+              .Flatten(child => child.Children)
+              .Where(child =>
+                         child.Symbol.Kind is not SymbolKind.Assembly
+                             and not SymbolKind.Namespace
+                             and not SymbolKind.NamedType)
+              .Select(m => (Metric: m, m.CyclomaticComplexity))
+              .OrderByDescending(_ => _.CyclomaticComplexity)
+              .Select(_ => (_.CyclomaticComplexity, _.Metric.ToCyclomaticComplexityEmoji()))
+              .FirstOrDefault();
 
-    static IEnumerable<TSource> Flatten<TSource>(
-        this IEnumerable<TSource> parent, Func<TSource, IEnumerable<TSource>> childSelector) =>
+    static IEnumerable<TSource> Flatten<TSource>(this IEnumerable<TSource> parent, Func<TSource, IEnumerable<TSource>> childSelector) =>
         parent.SelectMany(
-            source => childSelector(source).Flatten(childSelector))
-            .Concat(parent);
+                  source => childSelector(source).Flatten(childSelector))
+              .Concat(parent);
 
-    internal static string ToMermaidClassDiagram(
-        this CodeAnalysisMetricData classMetric, string className)
-    {
+    internal static string ToMermaidClassDiagram(this CodeAnalysisMetricData classMetric, string className) {
         // https://mermaid-js.github.io/mermaid/#/classDiagram
-        StringBuilder builder = new("classDiagram");
+        StringBuilder builder = new ("classDiagram");
         builder.AppendLine();
 
         className = className.Contains(".")
-                    ? className.Substring(className.IndexOf(".") + 1)
-                    : className;
-        /*
-        string classNameId = className
-                .Replace("<", "_")
-                .Replace(">", "_")
-                .Replace(",", "_")
-                .Replace(" ", "_");
-        string classNameDisplay = className
-                .Replace("<", "&lt;")
-                .Replace(">","&gt;");
-        
-        string classNameEscaped = 
-            "`"
-            + className
-                .Replace("<", "&lt;")
-                .Replace(">","&gt;")
-            + "`";
-        */
+            ? className[(className.IndexOf(".", StringComparison.Ordinal) + 1)..]
+            : className;
 
         if (classMetric.Symbol is ITypeSymbol typeSymbol &&
-            typeSymbol.Interfaces.Length > 0)
-        {
-            foreach (var @interface in typeSymbol.Interfaces)
-            {
+            typeSymbol.Interfaces.Length > 0) {
+            foreach (var @interface in typeSymbol.Interfaces) {
                 string interfaceName = @interface.Name;
                 if (@interface.IsGenericType) {
                     var typeArgs = string.Join(",", @interface.TypeArguments.Select(ta => ta.Name));
                     interfaceName = $"{@interface.Name}<{typeArgs}>";
                 }
-                builder.AppendLine($"{toClassNameId(interfaceName)} <|-- {toClassNameId(className)} : implements"); 
-                
+                builder.AppendLine($"{toClassNameId(interfaceName)} <|-- {toClassNameId(className)} : implements");
+
                 builder.AppendLine($$"""
-                    class {{toClassNameId(interfaceName)}} ["{{classNameToDisplay(interfaceName)}}"] {
-                        <<interface>>
-                    }
-                    """ );
+                                     class {{toClassNameId(interfaceName)}} ["{{classNameToDisplay(interfaceName)}}"] {
+                                         <<interface>>
+                                     }
+                                     """);
             }
         }
 
         builder.AppendLine(
             $$"""
-            class {{toClassNameId(className)}} ["{{classNameToDisplay(className)}}"] {
-            """ );
-            
-        
-        
-        static string? classNameToDisplay( string className ) =>
-             className
+              class {{toClassNameId(className)}} ["{{classNameToDisplay(className)}}"] {
+              """);
+
+
+        static string? classNameToDisplay(string className) =>
+            className
                 .Replace("<", "&lt;")
                 .Replace(">", "&gt;");
-        
-        
-        static string toClassNameId( string className ) =>
+
+
+        static string toClassNameId(string className) =>
             className
                 .Replace("<", "_")
                 .Replace(">", "_")
                 .Replace(",", "_")
                 .Replace(" ", "_")
                 .Replace("~", "_");
-        
+
         /*
         static string escapeClassName( string className ) =>
              '`'
@@ -122,102 +96,94 @@ static class CodeAnalysisMetricDataExtensions
                   .Replace(">", "&gt;")
              + '`';
         */
-        
+
         static string? ToClassifier(CodeAnalysisMetricData member) =>
-            (member.Symbol.IsStatic, member.Symbol.IsAbstract) switch
-            {
+            (member.Symbol.IsStatic, member.Symbol.IsAbstract) switch {
                 (true, _) => "$", (_, true) => "*", _ => null
             };
 
-        static string ToAccessModifier(CodeAnalysisMetricData member)
-        {
+        static string ToAccessModifier(CodeAnalysisMetricData member) {
             // TODO: figure out how to get access modifiers.
             // + Public
             // - Private
             // # Protected
             // ~ Package / Internal
 
-            return member.Symbol switch
-            {
-                IFieldSymbol field => "-",
-                IPropertySymbol prop => "+",
-                IMethodSymbol method => "+",
-                _ => "+"
-            };
+            return member.Symbol switch {
+                       IFieldSymbol field   => "-",
+                       IPropertySymbol prop => "+",
+                       IMethodSymbol method => "+",
+                       _                    => "+"
+                   };
         }
-        
-        static string toMemberSafeName(CodeAnalysisMetricData member, string className) =>
-            classNameToDisplay(ToMemberName( member, className));
 
-        static string ToMemberName(CodeAnalysisMetricData member, string className)
-        {
+        static string toMemberSafeName(CodeAnalysisMetricData member, string className) =>
+            classNameToDisplay(ToMemberName(member, className));
+
+        static string ToMemberName(CodeAnalysisMetricData member, string className) {
             var accessModifier = ToAccessModifier(member);
-            if (member.Symbol.Kind is SymbolKind.Method)
-            {
-                var method = member.ToDisplayName();
+            if (member.Symbol.Kind is SymbolKind.Method) {
+                var method     = member.ToDisplayName();
                 var ctorMethod = $"{className}.{className}";
-                if (method.StartsWith(ctorMethod))
-                {
+                if (method.StartsWith(ctorMethod)) {
                     var ctor = method.Substring(ctorMethod.Length);
                     return $"{accessModifier}.ctor{ctor} {className}";
                 }
 
-                if (member.Symbol is IMethodSymbol methodSymbol)
-                {
+                if (member.Symbol is IMethodSymbol methodSymbol) {
                     var rtrnType = methodSymbol.ReturnType.ToString()!;
-                    if (rtrnType.Contains("."))
-                    {
+                    if (rtrnType.Contains(".")) {
                         goto regex;
                     }
 
-                    var classNameOffset = className.Contains(".")
-                        ? className.Substring(className.IndexOf(".")).Length - 1
+                    var classNameOffset = className.Contains('.')
+                        ? className.Substring(className.IndexOf(".", StringComparison.Ordinal)).Length - 1
                         : className.Length;
 
-                    var index = rtrnType.Length + 2 + classNameOffset;
+                    var index           = rtrnType.Length + 2 + classNameOffset;
                     var methodSignature = method.Substring(index);
                     return $"{accessModifier}{methodSignature}{ToClassifier(member)} {rtrnType}";
                 }
 
-            regex:
-                Regex returnType = new(@"^(?<returnType>[\S]+)");
-                if (returnType.Match(method) is { Success: true } match)
-                {
+                regex:
+                Regex returnTypeRegex = new (@"^(?<returnType>\w+(?(<)[^\>]+>|[^ ]+))");
+                if (returnTypeRegex.Match(method) is { Success: true } match) {
                     // 2 is hardcoded for the space and "." characters
-                    var index = method.IndexOf(" ") + 2 + className.Length;
-                    var methodSignature = method.Substring(index);
-                    return $"{accessModifier}{methodSignature}{ToClassifier(member)} {match.Groups["returnType"]}";
+                    var index            = method.IndexOf($" {className}.", StringComparison.Ordinal) + 2 + className.Length;
+                    var methodSignature  = method.Substring(index);
+                    var memberClassifier = ToClassifier(member);
+                    var returnType       = match.Groups["returnType"];
+                    var line             = $"{accessModifier}{methodSignature}{memberClassifier} {returnType}";
+                    return line;
                 }
             }
 
             return $"{accessModifier}{member.ToDisplayName().Replace($"{className}.", "")}";
         }
-        
+
         var members = classMetric.Children.OrderBy(
-               m => m.Symbol.Kind switch
-               {
-                   SymbolKind.Field => 1,
-                   SymbolKind.Property => 2,
-                   SymbolKind.Method => 3,
-                   _ => 4
-               }).ToArray();
-        if ( members.Length == 0 ){
+            m => m.Symbol.Kind switch {
+                     SymbolKind.Field    => 1,
+                     SymbolKind.Property => 2,
+                     SymbolKind.Method   => 3,
+                     _                   => 4
+                 }).ToArray();
+        if (members.Length == 0) {
             builder.AppendLine("    "); // empty line for empty Type
         }
-        foreach (var member in members ) {
-            _ = member.Symbol.Kind switch
-            {
-                SymbolKind.Field => builder.AppendLine(
-                    $"    {toMemberSafeName(member, className)}{ToClassifier(member)}"),
+        foreach (var member in members) {
+            _ = member.Symbol.Kind switch {
+                    SymbolKind.Field => builder.AppendLine(
+                        $"    {toMemberSafeName(member, className)}{ToClassifier(member)}"),
 
-                SymbolKind.Property => builder.AppendLine(
-                    $"    {toMemberSafeName(member, className)}{ToClassifier(member)}"),
+                    SymbolKind.Property => builder.AppendLine(
+                        $"    {toMemberSafeName(member, className)}{ToClassifier(member)}"),
 
-                SymbolKind.Method => builder.AppendLine(
-                    $"    {toMemberSafeName(member, className)}"),
+                    SymbolKind.Method => builder.AppendLine(
+                        $"    {toMemberSafeName(member, className)}"),
 
-                _ => null
-            };
+                    _ => null
+                };
         }
 
         builder.AppendLine("}");
@@ -228,32 +194,29 @@ static class CodeAnalysisMetricDataExtensions
     }
 
     internal static string ToDisplayName(this CodeAnalysisMetricData metric) =>
-        metric.Symbol.Kind switch
-        {
+        metric.Symbol.Kind switch {
             SymbolKind.Assembly => metric.Symbol.Name,
 
             SymbolKind.NamedType => DisplayName(metric.Symbol),
 
             SymbolKind.Method
-            or SymbolKind.Field
-            or SymbolKind.Event
-            or SymbolKind.Property => metric.Symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
+                or SymbolKind.Field
+                or SymbolKind.Event
+                or SymbolKind.Property => metric.Symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
 
             _ => metric.Symbol.ToDisplayString()
         };
 
-    static string DisplayName(ISymbol symbol)
-    {
+    static string DisplayName(ISymbol symbol) {
         StringBuilder minimalTypeName =
-            new(
+            new (
                 symbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat));
 
         var containingType = symbol.ContainingType;
-        while (containingType is not null)
-        {
+        while (containingType is not null) {
             minimalTypeName.Insert(0, ".");
             minimalTypeName.Insert(0,
-                containingType.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat));
+                                   containingType.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat));
             containingType = containingType.ContainingType;
         }
 
--- DotNet.GitHubAction/Program.cs
+++ DotNet.GitHubAction/Program.cs
@@ -1,59 +1,50 @@
 ﻿using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) => services.AddGitHubActionServices())
-    .Build();
+                       .ConfigureServices((_, services) => services.AddGitHubActionServices())
+                       .Build();
 
 static TService Get<TService>(IHost host)
     where TService : notnull =>
     host.Services.GetRequiredService<TService>();
 
-static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
-{
-    using ProjectWorkspace workspace = Get<ProjectWorkspace>(host);
-    using CancellationTokenSource tokenSource = new();
+static async Task StartAnalysisAsync(ActionInputs inputs, IHost host) {
+    using ProjectWorkspace        workspace   = Get<ProjectWorkspace>(host);
+    using CancellationTokenSource tokenSource = new ();
 
-    Console.CancelKeyPress += delegate
-    {
-        tokenSource.Cancel();
-    };
+    Console.CancelKeyPress += delegate { tokenSource.Cancel(); };
 
     var projectAnalyzer = Get<ProjectMetricDataAnalyzer>(host);
 
-    Matcher matcher = new();
+    Matcher matcher = new ();
     matcher.AddIncludePatterns(new[] { "**/*.csproj", "**/*.vbproj" });
 
-    Dictionary<string, CodeAnalysisMetricData> metricData = new(StringComparer.OrdinalIgnoreCase);
-    var projects = matcher.GetResultsInFullPath(inputs.Directory);
+    Dictionary<string, CodeAnalysisMetricData> metricData = new (StringComparer.OrdinalIgnoreCase);
+    var                                        projects   = matcher.GetResultsInFullPath(inputs.Directory);
 
-    foreach (var project in projects)
-    {
+    foreach (var project in projects) {
         var metrics =
             await projectAnalyzer.AnalyzeAsync(
                 workspace, project, tokenSource.Token);
 
-        foreach (var (path, metric) in metrics)
-        {
+        foreach (var (path, metric) in metrics) {
             metricData[path] = metric;
         }
     }
 
-    var updatedMetrics = false;
-    var title = "";
-    StringBuilder summary = new();
-    if (metricData is { Count: > 0 })
-    {
-        var fileName = "CODE_METRICS.md";
-        var fullPath = Path.Combine(inputs.Directory, fileName);
-        var logger = Get<ILoggerFactory>(host).CreateLogger(nameof(StartAnalysisAsync));
+    var           updatedMetrics = false;
+    var           title          = "";
+    StringBuilder summary        = new ();
+    if (metricData is { Count: > 0 }) {
+        var fileName   = "CODE_METRICS.md";
+        var fullPath   = Path.Combine(inputs.Directory, fileName);
+        var logger     = Get<ILoggerFactory>(host).CreateLogger(nameof(StartAnalysisAsync));
         var fileExists = File.Exists(fullPath);
 
-        logger.LogInformation(
-            $"{(fileExists ? "Updating" : "Creating")} {fileName} markdown file with latest code metric data.");
+        logger.LogInformation("{Updating} {FileName} markdown file with latest code metric data", (fileExists ? "Updating" : "Creating"), fileName);
 
         summary.AppendLine(
             title = $"{(fileExists ? "Updated" : "Created")} {fileName} file, analyzed metrics for {metricData.Count} projects.");
 
-        foreach (var (path, _) in metricData)
-        {
+        foreach (var (path, _) in metricData) {
             summary.AppendLine($"- *{path}*");
         }
 
@@ -65,28 +56,23 @@ static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
 
         updatedMetrics = true;
     }
-    else
-    {
+    else {
         summary.Append("No metrics were determined.");
     }
 
     // https://docs.github.com/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
     // ::set-output deprecated as mentioned in https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
     var githubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT", EnvironmentVariableTarget.Process);
-    if (!string.IsNullOrWhiteSpace(githubOutputFile))
-    {
-        using (var textWriter = new StreamWriter(githubOutputFile!, true, Encoding.UTF8))
-        {
-            textWriter.WriteLine($"updated-metrics={updatedMetrics}");
-            textWriter.WriteLine($"summary-title={title}");
-            textWriter.WriteLine(
-                "summary-details<<EOF\n" +
-                summary +
-                "\nEOF\n" );
-        }
+    if (!string.IsNullOrWhiteSpace(githubOutputFile)) {
+        await using var textWriter = new StreamWriter(githubOutputFile!, true, Encoding.UTF8);
+        textWriter.WriteLine($"updated-metrics={updatedMetrics}");
+        textWriter.WriteLine($"summary-title={title}");
+        textWriter.WriteLine(
+            "summary-details<<EOF\n" +
+            summary                  +
+            "\nEOF\n");
     }
-    else
-    {
+    else {
         Console.WriteLine($"::set-output name=updated-metrics::{updatedMetrics}");
         Console.WriteLine($"::set-output name=summary-title::{title}");
         Console.WriteLine($"::set-output name=summary-details::{summary}");
@@ -95,14 +81,13 @@ static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
     Environment.Exit(0);
 }
 
-var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+var parser = Default.ParseArguments<ActionInputs>(() => new (), args);
 parser.WithNotParsed(
-    errors =>
-    {
+    errors => {
         Get<ILoggerFactory>(host)
             .CreateLogger("DotNet.GitHubAction.Program")
             .LogError(
-                string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
+                String.Join(Environment.NewLine, errors.Select(error => error.ToString())));
 
         Environment.Exit(2);
     });
