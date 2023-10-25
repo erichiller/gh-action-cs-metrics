3cc400061e16afb10676a13146f4604234f9e09e
Eric Hiller <mrraptor98@gmail.com>
2023-10-25 12:32:34 -0500

    update Class1.cs

--- test/SampleProject/SampleProject/Class1.cs
+++ test/SampleProject/SampleProject/Class1.cs
@@ -4,6 +4,13 @@ using System.Threading.Tasks;
 
 namespace SomeRoot.SampleProject;
 
+public interface IGenericOne<T1> {
+    public T1 GenericTOne { get; }
+}
+
+public interface IGenericTwo<T1,T2> : IGenericOne<T1> {
+    public T2 GenericTTwo { get; }
+}
 
 public interface IIntFoo {
     public int Foo { get; set; }
@@ -15,18 +22,20 @@ public class Class1 : IIntFoo {
     public Class2 Class2Prop { get; }
 }
 
-public class Class2 {
+public class Class2 : IGenericTwo<string, int> {
     public required string StringProp { get; init; }
     public string[] StringArrayProp { get; }
+    public string GenericTOne { get; }
+    public int GenericTTwo { get; }
 }
 
-public class ClassGeneric1<T> {
+public class ClassGeneric1<T> : IGenericOne<Class1> {
     public T MyT { get; }
+    public Class1 GenericTOne { get; }
     
     public ValueTask DoThingsAsync() =>
          ValueTask.CompletedTask;
          
     public IDisposable DisposableReturningMethod() =>
         throw new NotImplementedException();
-    
 }
